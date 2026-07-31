f8b782967e656c1e0c872700ffc62a2da4a4fee3
RDHT1010 <discretetimemarkovchain@gmail.com>
2026-07-31 17:48:53 +0700

    Update SCRIPT_CARD_ONLINE_V1.lua

--- SCRIPT_CARD_ONLINE_V1.lua
+++ SCRIPT_CARD_ONLINE_V1.lua
@@ -580,7 +580,7 @@ function Vatican_Goldpass()
     gg.TYPE_DWORD
   )
 
- gg.refineNumber("696B5328h", gg.TYPE_DWORD)
+ gg.refineNumber("696B532Ch", gg.TYPE_DWORD)
 
  local results = gg.getResults(10)
 
